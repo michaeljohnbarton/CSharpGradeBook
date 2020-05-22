2b3d05c351971290781d904789177ae88d4315e6
mbarton <MBarton@nextgen.com>
2020-05-22 13:50:22 -0400

    Initial commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
+
+    public class NamedObject
+    {
+        public NamedObject(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        String Name { get; }
+        event GradeAddedDelegate GradeAdded;
+    }
+
+    public abstract class Book : NamedObject, IBook
+    {
+        protected Book(string name) : base(name)
+        {
+        }
+
+        public abstract event GradeAddedDelegate GradeAdded;
+
+        public abstract void AddGrade(double grade);
+
+        public abstract Statistics GetStatistics();
+    }
+
+    public class InMemoryBook : Book
+    {
+        private List<double> grades;
+
+        readonly string category = "Science";
+        public const string CONST_CATEGORY = "Math";
+
+        public InMemoryBook(string name) : base(name)
+        {
+            grades = new List<double>();
+        }
+
+        public void AddGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
+        public override void AddGrade(double grade)
+        {
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+
+            for (var i = 0; i < grades.Count; i++)
+            {
+                result.Add(grades[i]);
+            }
+
+            return result;
+        }
+    }
+}
